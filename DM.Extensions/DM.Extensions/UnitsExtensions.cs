db2296fed795e670f85a7ed93ec91b942ada93f6
Dmytro Matsola <dmytro_matsola@symbotic.com>
2017-03-02 15:25:12 +0200

    Added inch-mm conversion extensions

---
+++ DM.Extensions/DM.Extensions/UnitsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DM.Extensions
+{
+    public static class UnitsExtensions
+    {
+        public static double InchesToMillimeters(this double inches)
+        {
+            return inches * 25.4;
+        }
+
+        public static double MillimetersToInches(this double millimeters)
+        {
+            return millimeters / 25.4;
+        }
+    }
+}
